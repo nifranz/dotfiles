c9f21ce5b434d861b6ab68af21a32f54d1b29e9a
niklasfranz <nf.app@icloud.com>
2023-04-10 11:36:20 +0200

    fix: ip.sh works on all sh interpreters now

--- .tmux/ip.sh
+++ .tmux/ip.sh
@@ -1,5 +1,5 @@
-if [[ $(uname) = "Darwin" ]]; then
+if [ $(uname) = "Darwin" ]; then
     ipconfig getifaddr en0
-elif [[ $(uname) = "Linux" ]]; then
+elif [ $(uname) = "Linux" ]; then
     hostname -I | awk '{print $1}'
 fi
