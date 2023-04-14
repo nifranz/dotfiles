048da15016af39347d04fdae8e4bf356c85cabe4
niklasfranz <nf.app@icloud.com>
2023-04-14 19:55:04 +0200

    feat: added lualine to nvim packer plugins

--- nvim/personal.nvimconfig/lua/plugins.lua
+++ nvim/personal.nvimconfig/lua/plugins.lua
@@ -2,6 +2,10 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use { 'catppuccin/nvim', as = 'catppuccin' }
 	use 'neovim/nvim-lspconfig'
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 	use 'edkolev/tmuxline.vim'
 	use 'nvim-zh/better-escape.vim'
 	use 'tpope/vim-commentary'
