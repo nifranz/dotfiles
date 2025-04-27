8d118fc8fa81dcd92ba5cbd8973cb962c544b823
nifranz <nifranz@uni-potsdam.de>
2025-04-27 13:36:27 +0200

    feat: add interactive docker commands

--- fish/functions/d-exec.fish
+++ fish/functions/d-exec.fish
@@ -1,7 +1,53 @@
 function d-exec
-    if test (count $argv) -eq 0
-        echo "Usage: d-exec <container_name>"
-        return 1
+    # Default shell is /bin/bash
+    set shell /bin/bash
+
+    # Check if --sh flag is passed and remove it from argv
+    if contains -- --sh $argv
+        set shell /bin/sh
+        # Remove --sh from the arguments
+        set argv (string join ' ' (string match -v -- '--sh' $argv))
     end
-    docker exec -it $argv[1] bash
+
+    # If no argument is passed, ask the user to select a container
+    if test (count $argv) -eq 0
+        set containers (docker ps --format "{{.Names}}")
+
+        # Check if there are any containers running
+        if test (count $containers) -eq 0
+            echo "No running containers found."
+            return 1
+        end
+
+        echo "Select a container:"
+
+        # List containers with their index (starting from 1)
+        for i in (seq (count $containers))
+            echo "$i. $containers[$i]"
+        end
+
+        # Prompt the user to enter a number
+        echo -n "Enter the number of the container: "
+        read choice
+
+        # Validate input: ensure it's a number and within the valid range
+        if not string match -qr '^[0-9]+$' -- $choice
+            echo "Invalid input. Please enter a valid number."
+            return 1
+        end
+
+        if test $choice -lt 1 -o $choice -gt (count $containers)
+            echo "Choice out of range."
+            return 1
+        end
+
+        # Get the container name based on the user's choice
+        set container $containers[$choice]
+    else
+        set container $argv[1]
+    end
+
+    # Run docker exec in the selected container with the appropriate shell
+    docker exec -it $container $shell
 end
+
--- fish/functions/d-logs.fish
+++ fish/functions/d-logs.fish
@@ -1,7 +1,41 @@
 function d-logs
+# If no argument is passed, ask the user to select a container
     if test (count $argv) -eq 0
-        echo "Usage: d-logs <container_name>"
-        return 1
+        set containers (docker ps --format "{{.Names}}")
+
+        # Check if there are any containers running
+        if test (count $containers) -eq 0
+            echo "No running containers found."
+            return 1
+        end
+
+        echo "Select a container:"
+
+        # List containers with their index (starting from 1)
+        for i in (seq (count $containers))
+            echo "$i. $containers[$i]"
+        end
+
+        # Prompt the user to enter a number
+        echo -n "Enter the number of the container: "
+        read choice
+
+        # Validate input: ensure it's a number and within the valid range
+        if not string match -qr '^[0-9]+$' -- $choice
+            echo "Invalid input. Please enter a valid number."
+            return 1
+        end
+
+        if test $choice -lt 1 -o $choice -gt (count $containers)
+            echo "Choice out of range."
+            return 1
+        end
+
+        # Get the container name based on the user's choice
+        set container $containers[$choice]
+    else
+        set container $argv[1]
     end
-    docker logs -f $argv[1]
+
+    docker logs -f $container 
 end
